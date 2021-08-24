2db023e9adfcca9dd9a26699ea1513ffab2e245f
Techtroll18-Jan <74965042+Techtroll18-Jan@users.noreply.github.com>
2021-08-23 22:44:37 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "BLUEPRINT";
+const accessCode2 = "SYNCHRONOUS";
+const accessCode3 = "VISIBILITY";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R N P B U I L E T", 100,50)
+    fill("lightblue")
+    text("Hint: class is a !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("U H N O Y S C R O S N", 700,150)
+    fill("lightblue")
+    text("Hint: default function !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text(" L Y I V B T I S I", 100,250)
+    fill("lightblue")
+    text("Hint: it makes an object hide !!", 100,270)
+
+}
